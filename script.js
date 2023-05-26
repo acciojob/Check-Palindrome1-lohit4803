ea782f878750b876c536f2317cc3e59423bbf637
lohit4803 <104892757+lohit4803@users.noreply.github.com>
2023-05-26 13:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	let s1 = str.trin()
+	let s2 = s1.toLowerCase()
+	let i=0
+	let j=s2.length-1
+	while(i<j){
+		if(s2.charAt(i) != s2.charAt(j)) return false
+		i++
+		j--
+	}
+	return true
 }
 module.exports = palindrome
